e5c06169a8b6c80ad25728c4d01d0afd9bd11015
HyunDaiKim <hkim0026@student.monash.edu>
2023-09-11 23:59:48 +0800

    (Name)Product Backlog
    added the button that can change the name of the product backlog

--- swif_app/src/ProductBacklog.jsx
+++ swif_app/src/ProductBacklog.jsx
@@ -1,13 +1,37 @@
+import { useState, useEffect } from 'react';
 import CreateTask from './CreateTask';
 
 const ProductBacklog = () => {
+    const defaultName = 'Product Backlog';
+    const storedName = localStorage.getItem('productName');
+    
+    const [productName, setProductName] = useState(storedName || defaultName);
+    const [editingName, setEditingName] = useState(false);
+
+    useEffect(() => {
+        // Save the product name to localStorage whenever it changes
+        localStorage.setItem('productName', productName);
+    }, [productName]);
+
     return (
         <>
-            <h1>Product Backlog</h1>
+            <h1>{productName}</h1>
+            {editingName ? 
+                <div>
+                    <input 
+                        type="text" 
+                        value={productName}
+                        onChange={(e) => setProductName(e.target.value)}
+                        style={{ fontSize: '1.5em', padding: '10px', width: '300px' }}  // Adjust these styles as desired
+                    />
+                    <button onClick={() => setEditingName(false)}>Confirm</button>
+                </div>
+            :
+                <button onClick={() => setEditingName(true)}>Change Name</button>
+            }
             <CreateTask />
         </>
     );
-
 };
 
 export default ProductBacklog;
