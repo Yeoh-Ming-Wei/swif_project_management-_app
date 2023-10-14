7f92c4fa05fbf07614b26b73fa2d397685654418
myeo0008 <myeo0008@student.monash.edu>
2023-10-15 04:01:18 +0800

    Card: Update card attributes to handle task and drag functions

--- swif_app/src/components/Card.jsx
+++ swif_app/src/components/Card.jsx
@@ -1,7 +1,10 @@
-const Card = ({id, title, taskStage, priority, storyPoints, editFunction, deleteFunction}) => {
+const Card = ({task, editFunction, deleteFunction, onDrag, onDragEnd}) => {
+    const prioritySelection = ["Low", "Medium", "Important", "Urgent"];
+    const storyPointSelection = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+    const taskStageSelection = ["Planning", "Development", "In Progress", "Completed"];
 
     const bgColours = (priority) => {
-      switch(priority) {
+      switch(prioritySelection[priority]) {
         case "Urgent": 
             return "Salmon"
         case "Important":
@@ -16,17 +19,28 @@ const Card = ({id, title, taskStage, priority, storyPoints, editFunction, delete
     }
 
     return (
-        <div id={id} className={"card"}  style={{width: '18rem', height: '10rem', margin: "10px", padding: "10px", backgroundColor: bgColours(priority), color: "black", borderRadius: "16px"}}>
+        <div 
+            id={task.id} 
+            className={"card"}  
+            draggable = {!!(onDrag !== undefined && onDragEnd !== undefined)}
+            onDrag={onDrag}
+            onDragEnd={onDragEnd}
+            style={{width: '18rem', height: '10rem', margin: "10px", padding: "10px", backgroundColor: bgColours(task.priority), color: "black", borderRadius: "16px"}}
+        >
             <div className="card-body" style={{display:"flex", flexDirection:"column", justifyContent:"space-between"}}>
-            <h3 className="card-title" align="left">{title}</h3>
+            <h3 className="card-title" align="left">{task.name}</h3>
             
             <div style={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
-                <p className="card-text" align="left">{taskStage}</p>
-                <h3 className="card-text" align="right">{storyPoints}</h3>
+                <p className="card-text" align="left">{taskStageSelection[task.taskStage]}</p>
+                <h3 className="card-text" align="right">{storyPointSelection[parseInt(task.storyPoints)]}</h3>
             </div>
             <div style={{display:"flex", flexDirection:"row", justifyContent:"end"}}>
+                {editFunction !== undefined && deleteFunction !== undefined ? 
+                <>
                 <button onClick={editFunction}>Edit</button>
                 <button onClick={deleteFunction}>Delete</button>
+                </> : <></>}
+                
                 
             </div>
             
