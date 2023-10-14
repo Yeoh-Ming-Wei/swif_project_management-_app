0a29d8ee152261ded192a4d870640dd84325fbf8
myeo0008 <myeo0008@student.monash.edu>
2023-10-15 03:58:44 +0800

    Sprint Manager: Added new product to sprint table with draggable task

--- swif_app/src/SprintManager.jsx
+++ swif_app/src/SprintManager.jsx
@@ -2,19 +2,57 @@ import FunctionalButton from "./components/buttons/functionalbutton";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from 'react';
 
+import Card from "./components/Card";
+
 const SprintManager = () => {
+
     const p = JSON.parse(localStorage.getItem("projects"))
     const activeP = p.projects.filter(project => project.id == p.activeProject)[0]
+    const activeSprintBacklog = activeP.sprints.filter(sprint => sprint.id == p.activeSprint)[0]
 
-    const restoreTask = () => {
-        console.log("Restoring Task")
-        return activeP.sprint
+    const [sprintTask, setActiveSprintTask] = useState(activeSprintBacklog)
+    const [draggedTask, setDraggedTask] = useState(null);
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        console.log("updating dragged task to local storage", draggedTask);
+        localStorage.setItem("draggedTask", JSON.stringify(draggedTask)); // convert to string before storing
+    }, [draggedTask]);
+
+
+    const dragTask = (event, task) => {
+        console.log('PB dragging');
+        const currentDragged = JSON.parse(localStorage.getItem("draggedTask"));
+        console.log(currentDragged);
+        setDraggedTask(task);
     }
 
-    const [sprintTask, setActiveSprintTask] = useState()
-    const navigate = useNavigate()
+    const dragEnd = (event) => {
+        console.log('**SB drag END');
+        setDraggedTask(null);
+    }
+
     return (
+        <>
         <FunctionalButton text = "View Sprint Board" func = {() => navigate("/sprint_board")}/>
+
+        <div style={{display:"flex", flexDirection:"row", justifyContent:"center", }}>
+            <div width={"400px"} style={{border:"2px solid grey"}}>
+                <div style={{width:"300px"}}/>
+                <h2>Product Backlog</h2>
+                {activeP.tasks.map(t => { return ( <Card task = {t} key = {t.id} onDrag = {(event) => {dragTask(event, t)}} onDragEnd = {dragEnd}/> )})}
+            </div>
+            
+            <div style={{border:"2px solid grey"}}>
+                <div style={{width:"300px"}}/>
+                <h2>Sprint Backlog</h2>
+
+            </div>  
+        </div>
+        </>
+        
+
     )
     
 }
